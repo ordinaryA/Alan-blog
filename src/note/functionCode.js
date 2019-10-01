061283c3d083986b6f022cb79ed997750ead6265
ordinaryA <353458582@qq.com>
2019-10-01 17:49:05 +0800

    10.1

--- src/note/functionCode.js
+++ src/note/functionCode.js
@@ -1,5 +1,6 @@
 /**
  * 函数式编程
+ * @http 原文链接 https://juejin.im/post/5d8f0866e51d4578110dc765
  */
 
 // 我要做2+3、2+4的操作
@@ -14,12 +15,32 @@ var calc = a => b => a + b
 var addA = calc(2)
 addA(3) || addA(4)
 
-//解释
-//1.0
-calc = (a) => {
-    return (b) => a + b;
+{
+    //解释
+    //1.0
+    var calc = (a) => {
+        return (b) => a + b;
+    }
+    //2.0
+    var addA = (b) => 2 + b;
+    //3.0
+    addA(3) || addA(4);
 }
-//2.0
-addA = (b) => 2 + b;
-//3.0
-addA(3) || addA(4);
+
+// 一句话概括一下： 在做不同的事情的情况下，可能是基于某一个相同的条件
+
+// 非函数式
+var a = 1;
+function calc(){
+    ++a
+}
+calc()
+
+// 函数式
+var a = 1;
+function calc(num){
+    return ++num
+}
+calc(a)
+
+// 一句话概括一下：不要让你的函数，去影响到外部的变量
