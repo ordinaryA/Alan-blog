142d1f4677e4b70378060ce945d8346a3e214db1
ordinaryA <353458582@qq.com>
2019-10-04 14:52:01 +0800

    just commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,61 +1,15 @@
 module.exports = {
-    title: '个人主页',
-    description: 'Alan的博客',
+    title: 'Alan Blog', // 页签标题 : A001_VuePress博客搭建的简单教程&问题分析 # | Wiki 1001
+    description: 'Alan Blog', // meta 中的描述文字，意义不大，SEO用
+    // 注入到当前页面的 HTML <head> 中的标签
     head: [
-        ['link', {
-            rel: 'icon',
-            href: '/img/logo.ico'
-        }],
-        ['link', {
-            rel: 'manifest',
-            href: '/manifest.json'
-        }],
+        // 增加一个自定义的 favicon(网页标签的图标)
+        // 这里的 '/' 指向 docs/.vuepress/public 文件目录 
+        // 即 docs/.vuepress/public/img/geass-bg.ico
+        ['link', { rel: 'icon', href: '/img/geass-bg.ico' }], 
     ],
-    themeConfig: {
-        nav: [{
-                text: '主页',
-                link: '/'
-            },
-            {
-                text: '博文',
-                items: [{
-                        text: 'Android',
-                        link: '/android/'
-                    },
-                    {
-                        text: 'ios',
-                        link: '/ios/'
-                    },
-                    {
-                        text: 'Web',
-                        link: '/web/'
-                    }
-                ]
-            },
-            {
-                text: '关于',
-                link: '/about/'
-            },
-            {
-                text: 'Github',
-                link: 'https://www.github.com/codeteenager'
-            },
-        ],
-        sidebar: {
-            '/android/': [
-                "",
-                "android1",
-            ],
-            "/ios/": [
-                "",
-                "ios1",
-            ],
-            "/web/": [
-                "",
-                "web1",
-            ],
-        },
-        sidebarDepth: 2,
-        lastUpdated: 'Last Updated',
+    base: '/Wiki1001Pro/', // 这是部署到github相关的配置
+    markdown: {
+        lineNumbers: true // 代码块显示行号
     },
 }
