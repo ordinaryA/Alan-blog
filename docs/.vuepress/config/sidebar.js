8283c884c626153cad2db6aa2670f24dad437afb
ordinaryA <353458582@qq.com>
2019-10-14 18:28:34 +0800

    deploy

--- docs/.vuepress/config/sidebar.js
+++ docs/.vuepress/config/sidebar.js
@@ -1,22 +1,10 @@
-// const 代码之外的生存指南 = [
-//     '',
-//     ['软技能-代码之外的生存指南1（职业篇）.md', '代码之外的生存指南1（职业篇）'],
-// ];
-
-// const NodeJS = [{
-//     "title": "命令行交互",
-//     "collapsable": false,
-//     "children": [
-//         ["01.命令行交互/01.玩转Nodejs命令行.md", "玩转Nodejs命令行"]
-//     ]
-// }];
-
 const 大前端 = [{
         title: "JavaScript",
         collapsable: false,
         children: [
-            ['01.Javascript/01.正则表达式.md', '正则表达式'],
-        ]
+            ['01.Javascript/01.函数式编程.md', '函数式编程'],
+            ['01.Javascript/02.正则表达式.md', '正则表达式'],
+        ],
     },
     {
         title: "ES6",
