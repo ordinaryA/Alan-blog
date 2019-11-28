d58284599eae69c677151c0b809668c1e3226633
ordinaryA <353458582@qq.com>
2019-11-28 17:08:30 +0800

    deploy

--- docs/.vuepress/plugins/comment.js
+++ docs/.vuepress/plugins/comment.js
@@ -3,11 +3,11 @@ const comment = [
     {
         choosen: 'gitalk',
         options: {
-            clientID: '6558fdf298fa596c5d8c',
-            clientSecret: process.env.clientSecret || '',
-            repo: 'blog',
-            owner: 'dongyuanxin',
-            admin: ['dongyuanxin'],
+            clientID: '5dd39f85e86ae8e82929',
+            clientSecret: '403c1b0eef69f013c04f3846d813e8456afe872d',
+            repo: 'Alan-blog',
+            owner: 'ordinaryA',
+            admin: ['ordinaryA'],
             id: '<%- frontmatter.commentid || frontmatter.permalink %>',      // Ensure uniqueness and length less than 50
             distractionFreeMode: false,  // Facebook-like distraction free mode
             labels: ['Gitalk', 'Comment'],
